d10d11d77542aec29606b68068c6b9885fe0a65a
deepan <deepanappu1999@gmail.com>
2025-12-26 19:18:36 +0530

    config the files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { App } from './app';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {
+        path:'',
+        loadComponent:() => loadRemoteModule('mfe1','./Component').then((c) => c.App)
+    }
+];
